4e4bbef005a87ea105361a821aad86c03b6525c8
Melissa Chinnick <chinm4045@wrdsb.ca>
2017-11-27 08:30:40 -0500

    Test to add a new file

---
+++ Test Playground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// this playground is to test code, and make sure it can be put in git
+
+print(str)
