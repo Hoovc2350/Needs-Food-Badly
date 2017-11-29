5208e60bac51df10f21c0fb0fa8c40a1c9982b78
Melissa Chinnick <chinm4045@wrdsb.ca>
2017-11-29 08:56:41 -0500

    Removed test gauntlet.zip, created a sample map, created a point file

---
+++ Gauntlet/Gauntlet/Point Value.swift
@@ -0,0 +1,18 @@
+//
+//  Point Value.swift
+//  Gauntlet
+//
+//  Created by Melissa Chinnick on 2017-11-29.
+//  Copyright © 2017 Needs Food Badly. All rights reserved.
+//
+
+import Foundation
+
+/* Destroying enemy generator = 5pts
+   Defeating enemy = 10 pts
+   Eating food = 15 pts
+   Treasure chests = 100 pts
+   Keys = 100 pts
+   Thief's gold = 500 pts
+   Finding exit = ?? pts
+*/
